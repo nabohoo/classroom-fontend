33a00a75bb661a3c4de1185bcba9f3e5f9ce35c2
unknown <vpasuth@gmail.com>
2026-04-12 13:10:39 +0700

    initial commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>Dashborad</div>
+    )
+}
+export default Dashboard;
---
+++ src/pages/subjects/create.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SubjectsListCreate = () => {
+    return (
+        <div>SubjectsListCreate</div>
+    )
+}
+export default SubjectsListCreate
